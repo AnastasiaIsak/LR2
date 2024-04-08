233251238f23418ccdeb42d60f23b8b446f4619d
AnastasiaIsak <Isaanastacia@yandex.ru>
2024-04-08 18:47:30 +0300

    добавлен вывод сообщения в дополнительной ветке

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Это сделано на ветке Иванов_Сергей");
+        }
     }
 }
